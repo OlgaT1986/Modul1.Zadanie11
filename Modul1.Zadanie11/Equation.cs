0b36ef904fc76e9f2d9d89be08a5797129d19493
Admin <Admin@ADMIN->
2022-01-19 15:55:10 +0700

    Добавьте файлы проекта.

---
+++ Modul1.Zadanie11/Equation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie11
+{
+    struct Equation
+    {
+        //закрытое поле структуры
+        private double k;    
+        private double b;
+        //открытый конструктор - установка значения
+        public Equation(double k, double b) 
+        {
+            // обращение к полю
+            this.k = k; 
+            this.b = b;
+        }
+        //метод для решения, аргументы b, k из структуры
+        public string Root() 
+        {
+            double x = b / k;
+            if (k == 0)
+            return "На 0 делить нельзя";
+            return $"Решение: x = {x:0.00}";
+        }
+
+    }
+}
+
---
+++ Modul1.Zadanie11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решение линейного уравнения 0=kx+b");
+            //ввод числовых коэффициентов
+            Console.Write("Введите числовой коэффициент уравнения b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите числовой коэффициент уравнения k=");
+            double k = Convert.ToDouble(Console.ReadLine());
+            //создание экземпляра структуры
+            Equation equation = new Equation(k, b);
+            //вызов метода Root - метод вернет строку
+            Console.WriteLine(equation.Root());
+            Console.ReadKey();
+        }
+    }
+}
+
